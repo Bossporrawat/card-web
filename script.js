95a367d1dfa0ad1df187a37503d8b9b9c25db384
Bossporrawat <porrawat.tee@dome.tu.ac.th>
2024-07-06 02:13:30 +0700

    0.1.3

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function getRandomCard(numCards) {
-    // สัญลักษณ์ของไพ่
-    const suits = ['D', 'S', 'H', 'C'];
-    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
-    for (let i = 0; i < numCards; i++) {
-        const randomSuitIndex = Math.floor(Math.random() * 4);
-        const randomRankIndex = Math.floor(Math.random() * 13);
-        const selectedSuit = suits[randomSuitIndex];
-        const selectedRank = ranks[randomRankIndex];
-
-        console.log(`Random Card ${i + 1}: ${selectedRank}${selectedSuit}`);
-    }
-}
 // ในไฟล์ script.js
 import fs from 'fs';
 
